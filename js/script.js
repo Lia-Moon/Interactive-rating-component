ad3875c6fe26a6c49aba7e8b99d9a02a96df919d
jujusvieira <livrosquefazemsonhar@gmail.com>
2023-06-15 23:20:41 -0300

    Inclusão de arquivo javascript

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* const opt1 = document.querySelector('.option1');
+const opt2 = document.querySelector('.option2');
+const opt3 = document.querySelector('.option3');
+const opt4 = document.querySelector('.option4');
+const opt5 = document.querySelector('.option5');
+
+opt1.addEventListener("click", () => {
+    opt1.style.backgroundColor = "red";
+})
+
+opt2.addEventListener("click", () => {
+    opt2.style.backgroundColor = "red";
+})
+
+opt3.addEventListener("click", () => {
+    opt3.style.backgroundColor = "red";
+})
+
+opt4.addEventListener("click", () => {
+    opt4.style.backgroundColor = "red";
+})
+
+opt5.addEventListener("click", () => {
+    opt5.style.backgroundColor = "red";
+})
+
+const checkForClass = document.querySelector('rating__state--content--selection');
+checkForClass.classList.contains('.changeButtonColor');
+
+if(checkForClass.classList.contains('.changeButtonColor')){
+    classList.remove('.changeButtonColor');
+} else {
+    classList.add('.changeButtonColor');
+} */
